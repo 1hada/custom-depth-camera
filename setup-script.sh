689b961153e585997bbb94e6cfc18497fa0f072c
jonathan sanabria <jonsanria@gmail.com>
2024-01-03 03:51:59 +0100

    confirmed different ros-humble-rclcpp is affected by rclcpp source ros isaac instad of regular ros2 apt sources.

--- setup-script.sh
+++ setup-script.sh
@@ -1,3 +1,5 @@
 sudo apt-get install -y  tmuxp
 sudo apt-get install -y ros-humble-usb-cam
 # ideall we could install like this but currently the available debian package does not allow some of the nodes to launch properly # sudo apt-get install -y ros-humble-image-pipeline
+
+pip install -U colcon-common-extensions # i found the jetson nano needed this o be installed seperately
